8a7c1a5e8d9e8b628985e3fe4424b7a662e5a34a
paddi001 <kontakt.paddi001@gmail.com>
2025-09-18 20:47:48 +0200

    add Overlay server with Socket.IO and poll management

--- Server.js
+++ Server.js
@@ -1,58 +1,34 @@
 const express = require("express");
-const path = require("path");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
+
+const PORT = 4000;
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-app.use(express.static(path.join(__dirname, "Overlay")));
+let currentPoll = null;
 
+app.use(express.static("Overlay"));
 
-let polls = [];
+app.get("/poll", (req, res) => res.json(currentPoll || {}));
 
-app.get("/poll", (req, res) => {
-  if (!polls.length) return res.json({});
-  res.json(polls[0]);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "Overlay", "index.html"));
 });
 
 io.on("connection", (socket) => {
   console.log("Overlay connected");
-  socket.emit("updatePolls", polls);
+  socket.emit("updatePolls", currentPoll ? [currentPoll] : []);
 });
 
-function startPoll(poll) {
-  polls = [poll];
-  io.emit("updatePolls", polls);
+function updatePoll(poll) {
+  currentPoll = poll;
+  io.emit("updatePolls", currentPoll ? [currentPoll] : []);
 }
 
-function stopPoll() {
-  polls = [];
-  io.emit("updatePolls", polls);
-}
+server.listen(PORT, () => console.log(`Overlay Server läuft auf http://localhost:${PORT}`));
 
-function updateVotes(updatedPoll) {
-  if (!polls.length) return;
-  polls[0].votes = updatedPoll.votes;
-  io.emit("updatePolls", polls);
-}
-
-function getCurrentPoll() {
-  return polls[0] || null;
-}
-
-
-const PORT = 3002;
-server.listen(PORT)
-  .on('error', (err) => {
-    if (err.code === 'EADDRINUSE') {
-      console.error(`Port ${PORT} ist bereits belegt!`);
-    } else {
-      console.error(err);
-    }
-  })
-  .on('listening', () => console.log(`Overlay server running at http://localhost:${PORT}`));
-
-
-module.exports = { startPoll, stopPoll, updateVotes, getCurrentPoll };
+module.exports = { updatePoll };
