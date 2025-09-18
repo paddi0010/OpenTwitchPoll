d470e98710ae9e8b0ce34face4473b1f0c82ce73
paddi001 <kontakt.paddi001@gmail.com>
2025-09-18 20:50:19 +0200

    edited comments

--- Overlay/overlay.js
+++ Overlay/overlay.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:4000"); // Muss zu deinem Server-Port passen
+const socket = io("http://localhost:4000");
 
 const questionEl = document.getElementById("question");
 const optionsEl = document.getElementById("options");
