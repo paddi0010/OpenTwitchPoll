27e04d0ab627fc366ae61ecb8a7088da15de80df
paddi001 <kontakt.paddi001@gmail.com>
2025-09-19 16:37:02 +0200

    beginning implemnting clearPollCommand

--- commands/helpCommand.js
+++ commands/helpCommand.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "help",
-  description: "Show all available commands",
+  description: "Show all available command",
   execute(client, channel, tags, args, currentPoll) {
     client.say(channel, "Commands: !poll, !vote, !close, !clear, !list, !help");
     return { currentPoll };
