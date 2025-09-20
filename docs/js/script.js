ee40581c445b7a781650f6b864720030ecf408af
paddi001 <kontakt.paddi001@gmail.com>
2025-09-20 20:50:54 +0200

    -

--- docs/js/script.js
+++
@@ -1,8 +0,0 @@
-console.log("Webseite für mein Tool läuft!");
-
-// Beispiel: Button klick
-document.querySelectorAll("button").forEach(btn => {
-  btn.addEventListener("click", () => {
-    alert("Danke für dein Interesse!");
-  });
-});
