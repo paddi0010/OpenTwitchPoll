5b2d8623b9d2a3d963f3c69fb9abc66dd2c9c0aa
paddi001 <kontakt.paddi001@gmail.com>
2025-09-21 20:21:50 +0200

    -

--- Server.js
+++ Server.js
@@ -11,19 +11,24 @@ const io = new Server(server, { cors: { origin: "*" } });
 
 let currentPoll = null;
 
+// Overlay statische Dateien
 app.use(express.static("Overlay"));
 
+// API für aktuelle Poll
 app.get("/poll", (req, res) => res.json(currentPoll || {}));
 
+// Index.html bereitstellen
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "Overlay", "index.html"));
 });
 
+// Socket.IO Events
 io.on("connection", (socket) => {
   console.log("Overlay connected");
   socket.emit("updatePolls", currentPoll ? [currentPoll] : []);
 });
 
+// Poll updaten
 function updatePoll(poll) {
   currentPoll = poll;
   io.emit("updatePolls", currentPoll ? [currentPoll] : []);
